b3bee9dc40fee6b34664c86dc1af7efa54d2ff6e
Rafael V. dos Santos <rvsantos00@gmail.com>
2020-02-25 01:39:31 -0300

    adding action show to the users controller with tests

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,20 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :find_user, only: [:show]
+
   def index
     @users = User.all
     render json: @users, status: :ok
+  end
+
+  def show
+    render json: @user, status: :ok
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    head 404
   end
 end
--- spec/request/api/v1/users_spec.rb
+++ spec/request/api/v1/users_spec.rb
@@ -19,4 +19,26 @@ describe 'Users API', type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe 'GET /users/:id' do
+    before { get "/users/#{user_id}", params: {}, headers: headers }
+
+    context 'when the record exist' do
+      it 'returns the user' do
+        expect(json_body[:id]).to eq user_id
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when the record does not exist' do
+      let(:user_id) { 1000 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+    end
+  end
 end
