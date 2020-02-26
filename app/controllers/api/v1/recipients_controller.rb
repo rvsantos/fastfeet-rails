8d809df0f0e65f5608043bf1f739cf8b3c331425
Rafael V. dos Santos <rvsantos00@gmail.com>
2020-02-26 08:48:56 -0300

    adding action show to the recipients controller with tests

--- app/controllers/api/v1/recipients_controller.rb
+++ app/controllers/api/v1/recipients_controller.rb
@@ -1,6 +1,18 @@
 class Api::V1::RecipientsController < ApplicationController
+  before_action :find_recipient, only: [:show]
+
   def index
     @recipients = Recipient.all
     json_response(@recipients)
+  end
+
+  def show
+    json_response(@recipient)
+  end
+
+  private
+
+  def find_recipient
+    @recipient = Recipient.find(params[:id])
   end
 end
--- spec/request/api/v1/recipients_spec.rb
+++ spec/request/api/v1/recipients_spec.rb
@@ -15,4 +15,26 @@ describe 'Recipients API', type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe 'Get /recipients/:id' do
+    before { get "/recipients/#{recipient_id}", params: {}, headers: headers }
+
+    context 'when recipient exist' do
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns json data with recipient' do
+        expect(json_body).to have_key(:street)
+      end
+    end
+
+    context 'when recipient does not exist' do
+      let(:recipient_id) { 1000 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+    end
+  end
 end
