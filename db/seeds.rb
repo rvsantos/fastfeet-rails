4e2d11cb9d96e026deb3932bff98cc09674d9c8b
Rafael V. dos Santos <rvsantos00@gmail.com>
2020-02-25 23:04:52 -0300

    adding seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(
+  name: 'Administrador',
+  email: 'admin@fastfeet.com',
+  password: '123456',
+  password_confirmation: '123456'
+)
