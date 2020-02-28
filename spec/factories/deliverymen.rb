aa320e005f33e761ec7da3d2d85219933106e591
Rafael V. dos Santos <rvsantos00@gmail.com>
2020-02-28 16:34:39 -0300

    remove avatar from factory and add new image avatar2

--- spec/factories/deliverymen.rb
+++ spec/factories/deliverymen.rb
@@ -2,6 +2,5 @@ FactoryBot.define do
   factory :deliveryman do
     name { Faker::Name.name }
     email { Faker::Internet.email }
-    avatar { fixture_file_upload('spec/fixtures/avatar.png') }
   end
 end
