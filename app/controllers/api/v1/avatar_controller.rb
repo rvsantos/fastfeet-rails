30d3a60a27cb6c054ebf0353ffd58b77c4ac6190
Rafael V. dos Santos <rvsantos00@gmail.com>
2020-02-29 11:55:10 -0300

    method fix to updating avatar

--- app/controllers/api/v1/avatar_controller.rb
+++ app/controllers/api/v1/avatar_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::AvatarController < ApplicationController
   def create
     @deliveryman = Deliveryman.find(params[:deliveryman_id])
+    @deliveryman.avatar.purge if @deliveryman.avatar.attached?
     @deliveryman.avatar.attach(params[:avatar])
     json_response(@deliveryman.avatar.blob)
   end
