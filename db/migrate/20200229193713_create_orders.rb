e30f96c3f178f32543af6b008e5a5a9522451367
Rafael V. dos Santos <rvsantos00@gmail.com>
2020-03-01 12:53:22 -0300

    generating new orders model with fix fields

--- db/migrate/20200229193713_create_orders.rb
+++ db/migrate/20200229193713_create_orders.rb
@@ -1,8 +1,8 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
-      t.belongs_to :recipient, foreign_key: true
-      t.belongs_to :deliveryman, foreign_key: true
+      t.references :recipient, foreign_key: true
+      t.references :deliveryman, foreign_key: true
       t.string :product
       t.datetime :canceled_at
       t.datetime :start_date
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -5,17 +5,14 @@ RSpec.describe Order, type: :model do
 
   let(:order) { build(:order) }
 
-  it { is_expected.to respond_to(:deliveryman) }
-  it { is_expected.to respond_to(:recipient) }
+  it { is_expected.to belong_to(:recipient) }
+  it { is_expected.to belong_to(:deliveryman) }
+
   it { is_expected.to respond_to(:product) }
-  it { is_expected.to respond_to(:start_date) }
-  it { is_expected.to respond_to(:end_date) }
-  it { is_expected.to respond_to(:canceled_at) }
+  it { is_expected.to respond_to(:recipient) }
+  it { is_expected.to respond_to(:deliveryman) }
 
   it { is_expected.to validate_presence_of(:product) }
-
-  it { is_expected.to belong_to(:deliveryman) }
-  it { is_expected.to belong_to(:recipient) }
 
   it { is_expected.to be_valid }
 end
